ae7833715235b559de62d56a2682d06582c9c4fa
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-25 20:37:03 +0200

    Update react kitu s novou strukturou

--- kits/react/src/index.jsx
+++ kits/react/src/index.jsx
@@ -1,28 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import './style.css';
-
-const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
-  );
-};
+import { HomePage } from './pages/HomePage';
+import './global.css';
 
 createRoot(
   document.querySelector('#app'),
-).render(<App />);
+).render(<HomePage />);
--- kits/react/vite.config.js
+++ kits/react/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
+  publicDir: '../public',
+  server: {
+    hmr: false,
+  },
+  build: {
+    outDir: '../dist',
+    modulePreload: false,
+  },
   plugins: [react()],
-})
+});
